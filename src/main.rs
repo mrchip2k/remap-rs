bb30ecc5d0b3a6234adf7767b8b2519ecb77e5dc
mrchip2k <alemetzen3@gmail.com>
2022-05-27 18:55:09 +0200

    1.1, aligned and neater output

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-use std::env;
-
+use std::{env, cmp::max};
+use pad::PadStr;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	
 	if args.len() != 1+5 {
-		print_info();
+		show_info();
 	} else {
-		let input   = args[1].parse::<f64>().expect("Input 1 is not a number.");
-		let in_min  = args[2].parse::<f64>().expect("Input 2 is not a number.");
-		let in_max  = args[3].parse::<f64>().expect("Input 3 is not a number.");
-		let out_min = args[4].parse::<f64>().expect("Input 4 is not a number.");
-		let out_max = args[5].parse::<f64>().expect("Input 5 is not a number.");
+		let input   = args[1].parse::<f64>().expect("\n\n/!\\ Argument 1 is not a number!\n\n");
+		let in_min  = args[2].parse::<f64>().expect("\n\n/!\\ Argument 2 is not a number!\n\n");
+		let in_max  = args[3].parse::<f64>().expect("\n\n/!\\ Argument 3 is not a number!\n\n");
+		let out_min = args[4].parse::<f64>().expect("\n\n/!\\ Argument 4 is not a number!\n\n");
+		let out_max = args[5].parse::<f64>().expect("\n\n/!\\ Argument 5 is not a number!\n\n");
 		
-		do_the_thing_xdlmao(input, in_min, in_max, out_min, out_max);
+		show_result(input, in_min, in_max, out_min, out_max);
 	}
 }
 
-fn print_info() {
+fn show_info() {
 	println!("Pass arguments to calculate. Order:");
 	println!("input, in min, in max, out min, out max.");
 	println!("Example:");
@@ -25,13 +25,35 @@ fn print_info() {
 	println!("> 50");
 }
 
-fn do_the_thing_xdlmao(input: f64, in_min: f64, in_max: f64, out_min: f64, out_max: f64) {
-	println!("Input: {input}");
-	println!("In:   {in_min} ... {in_max}");
-	println!("Out:  {out_min} ... {out_max}");
+fn show_result(input: f64, in_min: f64, in_max: f64, out_min: f64, out_max: f64) {
+	let result = remap(&input, &in_min, &in_max, &out_min, &out_max);
 	
-	println!("> {}", remap(&input, &in_min, &in_max, &out_min, &out_max));
+	// columns/rows of things to show
+	let in_min = in_min.to_string();
+	let out_min = out_min.to_string();
+	
+	let in_max = in_max.to_string();
+	let out_max = out_max.to_string();
+	
+	// finding how wide each column is
+	let min_col_width = max(in_min.chars().count(), out_min.chars().count());
+	let max_col_width = max(in_max.chars().count(), out_max.chars().count());
+	
+	// add spaces
+	let in_min = in_min.pad(min_col_width, ' ', pad::Alignment::Right, false);
+	let out_min = out_min.pad(min_col_width, ' ', pad::Alignment::Right, false);
+	
+	let in_max = in_max.pad(max_col_width, ' ', pad::Alignment::Right, false);
+	let out_max = out_max.pad(max_col_width, ' ', pad::Alignment::Right, false);
+	
+	println!("Input: {input}");
+	println!("");
+	println!(" In: {in_min} .. {in_max}");
+	println!("Out: {out_min} .. {out_max}");
+	println!("");
+	println!("= {result}");
 }
+
 
 fn remap(input: &f64, in_min: &f64, in_max: &f64, out_min: &f64, out_max: &f64) -> f64{
 	out_min + (input-in_min) / (in_max-in_min) * (out_max-out_min)
