dc183d99d603b5c3d51a94493ef2857f3ad9a160
mrchip2k <alemetzen3@gmail.com>
2022-09-23 23:46:11 +0200

    Fancy display

---
+++ src/display_result.rs
@@ -0,0 +1,107 @@
+use crate::ArgStruct;
+use colored::*;
+
+const ROW_COUNT: usize = 3;
+
+pub fn display_result(result: f64, arg_st: ArgStruct) {
+//// Create columns
+	let mut columns: Vec<Column> = Vec::new();
+	let col_explain_input = Column {
+		contents: [
+			"        ".to_string(),
+			"Input → ".bold().to_string(),
+			"        ".to_string()
+		],
+		width: 8
+	};
+	let col_explain_output = Column {
+		contents: [
+			"         ".to_string(),
+			" ← Output".bold().to_string(),
+			"         ".to_string()
+		],
+		width: 9
+	};
+	let col_middle = Column {
+		contents: [
+			" min ".to_string(),
+			" --> ".bold().to_string(),
+			" max ".to_string()
+		],
+		width: 5
+	};
+	let mut col_input = Column {
+		contents: [
+			arg_st.in_min.to_string(),
+			arg_st.input.to_string(),
+			arg_st.in_max.to_string()
+		],
+		width: 0 // unknown
+	};
+	col_input.calc_width();
+	let mut col_output = Column {
+		contents: [
+			arg_st.out_min.to_string(),
+			result.to_string(),
+			arg_st.out_max.to_string()
+		],
+		width: 0 // unknown
+	};
+	col_output.calc_width();
+//// Padding on data columns
+	for i in 0..ROW_COUNT {
+		let mut new = String::new();
+		let len = col_input.contents[i].chars().count();
+		let missing = col_input.width - len;
+		for i in 0..missing {
+			new.push(' ');
+		}
+		new.push_str(&col_input.contents[i]);
+		col_input.contents[i] = new;
+	}
+	for i in 0..ROW_COUNT {
+		let mut new = String::new();
+		let len = col_output.contents[i].chars().count();
+		let missing = col_output.width - len;
+		for i in 0..missing {
+			new.push(' ');
+		}
+		new.push_str(&col_output.contents[i]);
+		col_output.contents[i] = new;
+	}
+//// Bold on data columns
+	// this HAS to be done after counting the width,
+	// or else the style control characters get counted
+	// and it messes up the other rows alignment.
+	col_input.contents[1] = col_input.contents[1].bold().to_string();
+	col_output.contents[1] = col_output.contents[1].bold().to_string();
+//// Assembling the table
+	columns.push(col_explain_input);
+	columns.push(col_input);
+	columns.push(col_middle);
+	columns.push(col_output);
+	columns.push(col_explain_output);
+//// Rendering the table
+	for i in 0..ROW_COUNT {
+		let mut line_buf = String::new();
+		for col in &columns {
+			line_buf.push_str(&col.contents[i]);
+		}
+		println!("{}", line_buf);
+	}
+}
+
+struct Column {
+	contents: [String; ROW_COUNT],
+	width: usize,
+}
+impl Column {
+	fn calc_width(&mut self) {
+		for row in &self.contents {
+			let this_len = row.chars().count();
+			if this_len > self.width {
+				self.width = this_len;
+			}
+		}
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,72 @@
-use std::{env, cmp::max};
-use pad::PadStr;
+use std::{env::{self}};
+use colored::*;
+mod display_result;
+use display_result::*;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	
-	if args.len() != 1+5 {
-		show_info();
-	} else {
-		let input   = args[1].parse::<f64>().expect("\n\n/!\\ Argument 1 is not a number!\n\n");
-		let in_min  = args[2].parse::<f64>().expect("\n\n/!\\ Argument 2 is not a number!\n\n");
-		let in_max  = args[3].parse::<f64>().expect("\n\n/!\\ Argument 3 is not a number!\n\n");
-		let out_min = args[4].parse::<f64>().expect("\n\n/!\\ Argument 4 is not a number!\n\n");
-		let out_max = args[5].parse::<f64>().expect("\n\n/!\\ Argument 5 is not a number!\n\n");
-		
-		show_result(input, in_min, in_max, out_min, out_max);
+	let args = get_args();
+	match args {
+		None => {
+			println!("{}", "Pass arguments to calculate, in this order:".yellow().bold());
+			println!("{}, {}, {}, {}, {}.",
+				"input".purple(),
+				"in min".red(),
+				"in max".yellow(),
+				"out min".green(),
+				"out max".blue()
+			);
+			println!("Example:");
+			println!("remap {} {} {} {} {}",
+				"0.75".purple(),
+				"0".red(),
+				"1".yellow(),
+				"-100".green(),
+				"100".blue()
+			);
+			println!("Result: 50");
+		},
+		Some(arg_st) => {
+			let result = remap(
+				&arg_st.input,
+				&arg_st.in_min,
+				&arg_st.in_max,
+				&arg_st.out_min,
+				&arg_st.out_max
+			);
+			display_result(result, arg_st);
+		}
 	}
 }
 
-fn show_info() {
-	println!("Pass arguments to calculate. Order:");
-	println!("input, in min, in max, out min, out max.");
-	println!("Example:");
-	println!("remap 0.75 0 1 -100 100");
-	println!("> 50");
+pub struct ArgStruct {
+	input: f64,
+	in_min: f64,
+	in_max: f64,
+	out_min: f64,
+	out_max: f64
 }
 
-fn show_result(input: f64, in_min: f64, in_max: f64, out_min: f64, out_max: f64) {
-	let result = remap(&input, &in_min, &in_max, &out_min, &out_max);
-	
-	// columns/rows of things to show
-	let in_min = in_min.to_string();
-	let out_min = out_min.to_string();
-	
-	let in_max = in_max.to_string();
-	let out_max = out_max.to_string();
-	
-	// finding how wide each column is
-	let min_col_width = max(in_min.chars().count(), out_min.chars().count());
-	let max_col_width = max(in_max.chars().count(), out_max.chars().count());
-	
-	// add spaces
-	let in_min = in_min.pad(min_col_width, ' ', pad::Alignment::Right, false);
-	let out_min = out_min.pad(min_col_width, ' ', pad::Alignment::Right, false);
-	
-	let in_max = in_max.pad(max_col_width, ' ', pad::Alignment::Right, false);
-	let out_max = out_max.pad(max_col_width, ' ', pad::Alignment::Right, false);
-	
-	println!("Input: {input}");
-	println!("");
-	println!(" In: {in_min} .. {in_max}");
-	println!("Out: {out_min} .. {out_max}");
-	println!("");
-	println!("= {result}");
+fn get_args() -> Option<ArgStruct> {
+	let args_str: Vec<String> = env::args().collect();
+	if args_str.len() != 6 {
+		return None;
+	}
+	let mut args_float: Vec<f64> = Vec::new();
+	for arg in args_str[1..].iter() {
+		let parsed = arg.parse::<f64>();
+		if parsed.is_err() {
+			return None;
+		}
+		args_float.push(parsed.unwrap());
+	}
+	return Some(ArgStruct {
+		input: args_float[0],
+		in_min: args_float[1],
+		in_max: args_float[2],
+		out_min: args_float[3],
+		out_max: args_float[4]
+	});
 }
-
 
 fn remap(input: &f64, in_min: &f64, in_max: &f64, out_min: &f64, out_max: &f64) -> f64{
 	out_min + (input-in_min) / (in_max-in_min) * (out_max-out_min)
